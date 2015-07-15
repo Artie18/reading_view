76e0af072b1b60897652ef6de781762207bf1a6b
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-07-15 21:13:58 +0300

    Added article and children tags params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var readingView = require('./lib/reading_view');
 
-readingView.convert('http://new.disney.ru', {}, function (err, res) {
+
+readingView.convert('http://realt.onliner.by/2015/07/15/belchite/', {}, function (err, res) {
   console.log(err);
   console.log(res);
 });
