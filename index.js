b74283257e6c6efd333cfc404246f8332be472d6
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-07-16 00:10:24 +0300

    Added use function in index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var readingView = require('./lib/reading_view');
 
-
-readingView.convert('http://realt.onliner.by/2015/07/15/belchite/', {}, function (err, res) {
-  console.log(err);
-  console.log(res);
-});
+exports.convert = readingView.convert;
